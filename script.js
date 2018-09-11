5e1a623910d5be0fcef1e472a3a77913713ec162
Ron Mason <WHERESRONALD0@GMAIL.COM>
2018-09-11 15:02:54 -0400

    added click event play and time interval -not linked yet

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
-//need random # generator
-console.log(Math.floor(Math.random() * 4));
+const jerry = document.querySelector("#one");
+const elaine = document.querySelector("#two");
+const kramer = document.querySelector("#three");
+const george = document.querySelector("#four");
+console.log(jerry);
 
-//need intervals (slide show)
+// need random # generator
+const runGame = Math.floor(Math.random() * 4);
+console.log(runGame);
+
+// need time intervals between tile flashes (slide show) 
+setTimeout(function, 3000); 
+
+// need funtion that initiates game when user click play
+const play = document.querySelector("#play");
+play.addEventListener("click", runGame); //add random # gen function once I get it working
