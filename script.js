2c72efc2c61f953a12cf7b1c6b62ca7332ece657
Ron Mason <WHERESRONALD0@GMAIL.COM>
2018-09-12 13:08:39 -0400

    fixed numGen issue- put it outside of timer function

--- script.js
+++ script.js
@@ -3,78 +3,78 @@ const play = document.querySelector("#play");
 play.addEventListener("click", timerEng()); //add random # gen function once I get it working
 
 const numStore = [];
-let currentRnd = 0;
+let currentRnd = 1;
 
-function timerEng () {
-    setTimeout(function rndCount () {
-      for (let i = 0; i < currentRnd; i++) {
-        function runGame () { // winRndAlert resets numStore array
-          let numGen = Math.floor(Math.random() * 4)
-                  return numStore.push(numGen), foo(numGen) //need to trigger/push nunGen to tile funtionality
-              }
-      }
-    }, 5000)
-    return playerTurn()  
-  }
-  
+function randomNumGen() {
+  let numGen = Math.floor(Math.random() * 4);
+  return numStore.push(numGen), foo(numGen); //need to trigger/push nunGen to tile funtionality
+}
 
-const jerry = document.querySelector("#1");
-const elaine = document.querySelector("#2");
-const kramer = document.querySelector("#3");
-const george = document.querySelector("#4");
+function timerEng() {
+  setTimeout(function rndCount(currentRnd) {
+    for (let i = 0; i < currentRnd; i++) {
+      randomNumGen();
+    }
+  }, 5000);
+  return playerTurn();
+}
+
+const jerry = document.querySelector("#one");
+const elaine = document.querySelector("#two");
+const kramer = document.querySelector("#three");
+const george = document.querySelector("#four");
 const castPics = document.getElementsByClassName(".seinPics");
 
 function tile1Chg() {
-    setTimeout(function tileChg() {
-      jerry.style.borderColor = "red";
-    }, 1000);
-    setTimeout(function tileChg() {
-      jerry.style.borderColor = "black";
-    }, 1000);
-  }
-  
-  function tile2Chg() {
-    setTimeout(function tileChg() {
-      elaine.style.borderColor = "red";
-    }, 1000);
-    setTimeout(function tileChg() {
-      elaine.style.borderColor = "black";
-    }, 1000);
-  }
-  
-  function tile3Chg() {
-    setTimeout(function tileChg() {
-      kramer.style.borderColor = "red";
-    }, 1000);
-    setTimeout(function tileChg() {
-      kramer.style.borderColor = "black";
-    }, 1000);
-  }
-  
-  function tile4Chg() {
-    setTimeout(function tileChg() {
-      george.style.borderColor = "red";
-    }, 1000);
-    setTimeout(function tileChg() {
-      george.style.borderColor = "black";
-    }, 1000);
-  }
+  setTimeout(function tileChg() {
+    jerry.style.borderColor = "red";
+  }, 1000);
+  setTimeout(function tileChg() {
+    jerry.style.borderColor = "black";
+  }, 1000);
+} // do I need to "break"
 
-function foo(numGen) { 
-    if (numGen === 1) {
-      return tile1Chg();
-    } else if (numGen === 2) {
-      return tile2Chg();
-    } else if (numGen === 3) {
-      return tile3Chg();
-    } else if (numGen === 4) {
-      return tile4Chg();
-    }
+function tile2Chg() {
+  setTimeout(function tileChg() {
+    elaine.style.borderColor = "red";
+  }, 1000);
+  setTimeout(function tileChg() {
+    elaine.style.borderColor = "black";
+  }, 1000);
+}
+
+function tile3Chg() {
+  setTimeout(function tileChg() {
+    kramer.style.borderColor = "red";
+  }, 1000);
+  setTimeout(function tileChg() {
+    kramer.style.borderColor = "black";
+  }, 1000);
+}
+
+function tile4Chg() {
+  setTimeout(function tileChg() {
+    george.style.borderColor = "red";
+  }, 1000);
+  setTimeout(function tileChg() {
+    george.style.borderColor = "black";
+  }, 1000);
+}
+
+function foo(numGen) {
+  if (numGen === 1) {
+    return tile1Chg();
+  } else if (numGen === 2) {
+    return tile2Chg();
+  } else if (numGen === 3) {
+    return tile3Chg();
+  } else if (numGen === 4) {
+    return tile4Chg();
+  }
 }
 
 //player clicks this tile --> need to look at numGen array (check for )
 //
-
 
 let playerArr = [];
 
@@ -85,11 +85,12 @@ function playerTurn() {
       alert("NO SOUP FOR YOU!");
     } else if (numStore.length === playerArr.length) {
       for (let i = 0; i < numStore.length; i++) {
+        //Nested for loops array compare idea source came from W3Scools.com
         for (let j = 0; j < playerArr.length; j++) {
           if (playerArr[j] === numStore[i]) {
             return winRndAlert();
-          } else (playerArr[j] !== numStore[i]) {
-            alert("Game Over") 
+          } else {
+            alert("Yadda, Yadda... YOU LOSE");
           }
         }
       }
@@ -105,8 +106,8 @@ function playerTurn() {
         for (let j = 0; j < playerArr.length; j++) {
           if (playerArr[j] === numStore[i]) {
             return winRndAlert();
-          } else (playerArr[j] !== numStore[i]) {
-            alert("Game Over") 
+          } else {
+            alert("Yadda, Yadda... YOU LOSE");
           }
         }
       }
@@ -122,8 +123,8 @@ function playerTurn() {
         for (let j = 0; j < playerArr.length; j++) {
           if (playerArr[j] === numStore[i]) {
             return winRndAlert();
-          } else (playerArr[j] !== numStore[i]) {
-             alert("Game Over") 
+          } else {
+            alert("Yadda, Yadda... YOU LOSE");
           }
         }
       }
@@ -139,8 +140,8 @@ function playerTurn() {
         for (let j = 0; j < playerArr.length; j++) {
           if (playerArr[j] === numStore[i]) {
             return winRndAlert();
-          } else (playerArr[j] !== numStore[i]) {
-             alert("Game Over") 
+          } else {
+            alert("Yadda, Yadda... YOU LOSE");
           }
         }
       }
@@ -152,8 +153,8 @@ function playerTurn() {
 
 //need an alert function
 function winRndAlert() {
-    alert("You got it!. Let's move to the next round")
-    return currentRnd += 1, numStore = [], timerEng();//not sure this wil work
+  alert("You got it!. Let's move to the next round");
+  return (currentRnd += 1), (numStore = []), timerEng(); //not sure this wil work
 }
 
 //need reset button that resets the currentRnd and rndCount
